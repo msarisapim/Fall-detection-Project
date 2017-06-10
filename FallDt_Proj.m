41e5c560cd12998a9a00befb7580c5d6bb0eda46
msarisapim <msarisapim@gmail.com>
2017-06-10 20:55:31 +0700

    Add files via upload

--- FallDt_Proj.m
+++ FallDt_Proj.m
@@ -6,7 +6,7 @@ baseName = 'D:\Fall Detection' ;    VDO=1;  %change path name/ Input type
 foldername0 = 'Results'; mkdir(baseName,foldername0);
 %INPUT
 if VDO == 1 %%Video for input
-    vidObj=VideoReader('D:\Fall Detection\dataset\Le2i\Lecture room\video (2).avi');
+    vidObj=VideoReader('D:\Fall Detection\dataset\Le2i\Home_01\Videos\video (3).avi');
     vidHeight = vidObj.Height; vidWidth = vidObj.Width;
     vidName=vidObj.Name;  vidName=vidName(1,1:double(max(size(vidName)))-4);
     %collect each frame in s(structure)
@@ -34,20 +34,8 @@ else %%if Sequence of images for input
 end
 %create folder..
 	CreateFD;
-
-%%PROCESS%%
-
-%Create median filter
-BGsum=double(s(1).cdata);
-for i=1:k-1
-    Frame=double(s(i).cdata);
-    BGsum(:,:,1) = double(BGsum(:,:,1)+Frame(:,:,1));
-    BGsum(:,:,2) = double(BGsum(:,:,2)+Frame(:,:,2));
-    BGsum(:,:,3) = double(BGsum(:,:,3)+Frame(:,:,3));
-end
-BGMean=uint8(BGsum/k); %Median filter
-fBG=s(1).cdata; %BG filter(First BG)
-
+        
+               
 set(gcf,'position',[240 320 vidWidth vidHeight]);
 set(gca,'units','pixels');
 set(gca,'position',[0 0 vidWidth vidHeight]);
@@ -58,8 +46,25 @@ cd(foldername2)
 Kk=0; Ort1=0; Asp1=0; Ctx1=0; Cty1=0;countvid=0; Dat=0;
 Loop=k;
 
-for i=45:k-1
+BGsum=double(s(1).cdata);
+% %Create Mean filter as a bg
 % for i=1:k-1
+%           Frame=double(s(i).cdata); 
+%           BGsum(:,:,1) = double(BGsum(:,:,1)+Frame(:,:,1));
+%           BGsum(:,:,2) = double(BGsum(:,:,2)+Frame(:,:,2));
+%           BGsum(:,:,3) = double(BGsum(:,:,3)+Frame(:,:,3));
+% end       
+%           BGMean=uint8(BGsum/(i+1)); %Average bg 
+% for i=45:k-1
+for i=1:k-1
+         %Create Mean filter as a bg
+          Frame=double(s(i).cdata); 
+          BGsum(:,:,1) = double(BGsum(:,:,1)+Frame(:,:,1));
+          BGsum(:,:,2) = double(BGsum(:,:,2)+Frame(:,:,2));
+          BGsum(:,:,3) = double(BGsum(:,:,3)+Frame(:,:,3));
+          BGMean=uint8(BGsum/(i+1)); %Average bg 
+        fBG=s(1).cdata; %BG filter(First BG)
+        
         lim=k-1; countIP=i     
         num=int2str(i);
         imwrite(s(i).cdata,[vidName '-' num '.jpg'])
@@ -69,29 +74,29 @@ for i=45:k-1
         IS=imresize(BGMean,[240 320]);%first BG          
         
         %%BG subtraction  
-%       figure(1),subplot(3,3,1),imshow(Imfall)
         [Ibw,num]=BGsubtr(img,IS,fBG,num);
         
 %       figure(3),imshow(Ibw)
         imwrite(Ibw,[baseName '\' foldername4 '\' vidName '-' num '.jpg'])
-        hold off
+%         hold off
+
 %     if sum(sum(Ibw)) < 240*320/2              
+       [Ibw,Kk,k] = pcacaldat(Ibw,k,Kk,num);
+%       [Ibw,Dat,Kk,k,Ort1,Asp1] = pcacaldat(Ibw,k,Kk,num,Ort1,Asp1,Dat);
 %     
-%      [Ibw,Dat,Kk,k,Ort1,Asp1] = pcacaldat(Ibw,k,Kk,num,Ort1,Asp1,Dat);
 %     
-%     
-%     Frame_R=getframe(gcf);
-%     FrameResult=Frame_R.cdata;
-%     imwrite(FrameResult,[baseName '\' foldername6 '\' vidName '- ' num '.jpg'])
-%     countvid=countvid+1;
-%     VFrame.i=FrameResult;
-%     img=s(k-1).cdata;
-%     hold off  
+    Frame_R=getframe(gcf);
+    FrameResult=Frame_R.cdata;
+    imwrite(FrameResult,[baseName '\' foldername6 '\' vidName '- ' num '.jpg'])
+    countvid=countvid+1;
+    VFrame.i=FrameResult;
+    img=s(k-1).cdata;
+    hold off  
 %     end
 
   
 end
-xlswrite([baseName '\' foldername6 '\' vidName  '_Dat.xlsx'],Dat)
+% xlswrite([baseName '\' foldername6 '\' vidName  '_Dat.xlsx'],Dat)
 
 
 %%
